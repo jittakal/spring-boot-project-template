2a19425bd146aab4d461f15002e80d240cc46a2e
Jitendra Takalkar <Jitendra.Takalkar@nice.com>
2021-05-15 19:30:05 +0530

    project template for gradle build

---
+++ gradle/spring-boot-ms-template/README.md
@@ -0,0 +1,13 @@
+# Overview
+
+Sample Spring Boot micro-services project using gradle build tool.
+
+## Quick Commands
+
+```
+$ gradle build
+
+$ gradle :spring-boot-ms-template-application:bootRun
+
+$ curl http://127.0.0.1:8080/v1/hello
+```
---
+++ maven/spring-boot-ms-template/spring-boot-ms-template-application/src/test/java/io/jittakal/spring/boot/ms/template/application/controller/TemplateControllerIT.java
@@ -0,0 +1,38 @@
+package io.jittakal.spring.boot.ms.template.application.controller;
+
+import static org.assertj.core.api.Assertions.*;
+
+import java.net.URL;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.ResponseEntity;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class TemplateControllerIT {
+
+	@LocalServerPort
+	private int port;
+
+	private URL base;
+
+	@Autowired
+	private TestRestTemplate template;
+
+    @BeforeEach
+    public void setUp() throws Exception {
+        this.base = new URL("http://localhost:" + port + "/");
+    }
+
+    @Test
+    public void getTemplateHello() throws Exception {
+        ResponseEntity<String> response = template.getForEntity(base.toString() + "v1/hello",
+                String.class);
+        assertThat(response.getBody()).isEqualTo("Greetings from Micro-serivce Template based on Spring Boot!");
+    }
+}
