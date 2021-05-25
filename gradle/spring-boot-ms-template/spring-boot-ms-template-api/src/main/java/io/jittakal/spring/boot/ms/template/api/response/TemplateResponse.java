97c254d76619767c1435d741714cacdf7d8af94b
Jitendra Takalkar <Jitendra.Takalkar@nice.com>
2021-05-25 18:30:29 +0530

    Use Lombok library

--- gradle/spring-boot-ms-template/spring-boot-ms-template-api/src/main/java/io/jittakal/spring/boot/ms/template/api/response/TemplateResponse.java
+++ gradle/spring-boot-ms-template/spring-boot-ms-template-api/src/main/java/io/jittakal/spring/boot/ms/template/api/response/TemplateResponse.java
@@ -4,8 +4,14 @@ import java.io.Serializable;
 
 import com.fasterxml.jackson.databind.PropertyNamingStrategy;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import lombok.*;
 
 @JsonNaming(PropertyNamingStrategy.SnakeCaseStrategy.class)
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@EqualsAndHashCode
 public class TemplateResponse implements Serializable {
 
 	/**
@@ -14,47 +20,5 @@ public class TemplateResponse implements Serializable {
 	private static final long serialVersionUID = 6510134671819333233L;
 
 	private String templateFormat;
-	
-	public TemplateResponse() {
-		super();
-	}
-
-	public TemplateResponse(String templateFormat) {
-		super();
-		this.templateFormat = templateFormat;
-	}
-
-	public String getTemplateFormat() {
-		return templateFormat;
-	}
-
-	public void setTemplateFormat(String templateFormat) {
-		this.templateFormat = templateFormat;
-	}
-
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((templateFormat == null) ? 0 : templateFormat.hashCode());
-		return result;
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		TemplateResponse other = (TemplateResponse) obj;
-		if (templateFormat == null) {
-			if (other.templateFormat != null)
-				return false;
-		} else if (!templateFormat.equals(other.templateFormat))
-			return false;
-		return true;
-	}
 
 }
